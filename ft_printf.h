3ac8e3ed393b46e2f2b44496264ecb53fa9cf6dd
Jonathan LE QUERE <jle-quer@e3r9p12.42.fr>
2016-01-26 16:35:51 +0100

    Normaly check finish, Start impression

--- ft_printf.h
+++ ft_printf.h
@@ -6,14 +6,14 @@
 /*   By: jle-quer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/19 12:24:47 by jle-quer          #+#    #+#             */
-/*   Updated: 2016/01/22 20:57:06 by jle-quer         ###   ########.fr       */
+/*   Updated: 2016/01/26 16:33:54 by jle-quer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINT_F_H
 # define FT_PRINT_F_H
 
-# include "../Libft/libft.h"
+# include "../Libft/includes/libft.h"
 # include <stdarg.h>
 # include <stdio.h>
 
@@ -39,10 +39,12 @@ typedef struct	s_struct
 int				ft_printf(const char *format, ...);
 int				check_format(const char *format);
 int				check_options(const char format, t_struct *form);
-void			check_minimal_large(const char format, t_struct *form);
-void			check_precision(const char format, t_struct *form);
-void			check_size_modifier(const char *format, t_struct *form);
+int				check_type(const char format);
+int				check_minimal_large(const char format, t_struct *form);
+int				check_precision(const char format, t_struct *form);
+int				check_block(char *block, t_struct *form);
 
+void			check_size_modifier(const char *format, t_struct *form);
 void			init_struct(t_struct *form);
 
 #endif
