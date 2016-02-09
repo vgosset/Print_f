b7cc6dc832d7a5939ad5b6649e689cee4ec1168a
Jonathan LE QUERE <jle-quer@e1r1p9.42.fr>
2016-02-09 13:25:31 +0100

    Wchar to down.

--- Files/ft_putwchar.c
+++ Files/ft_putwchar.c
@@ -6,13 +6,35 @@
 /*   By: jle-quer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/29 12:30:53 by jle-quer          #+#    #+#             */
-/*   Updated: 2016/01/29 13:06:08 by jle-quer         ###   ########.fr       */
+/*   Updated: 2016/02/09 13:24:54 by jle-quer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_putwchar(char c)
+void	ft_putwchar(wchar_t c)
 {
-	;
+	char	*binary;
+
+	binary = ui_to_bin((unsigned int)c);
+	
+}
+
+char	*ui_to_bin(unsigned int u)
+{
+	char			*binary;
+	unsigned int	i;
+
+	if (!(binary == (char *)malloc(sizeof(char) * 33)))
+		return (NULL);
+	ft_bzero(binary, 33);
+	i = 31;
+
+	while (u > 0)
+	{
+		binary[i] = (u % 2) + '0';
+		u /= 2;
+		i--;
+	}
+	return (binary);
 }
--- check_display_block_c.c
+++ check_display_block_c.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_display_block_c.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jle-quer <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/02/09 12:27:03 by jle-quer          #+#    #+#             */
+/*   Updated: 2016/02/09 13:25:01 by jle-quer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
 void	check_display_block_c(t_struct *form, va_list va)
@@ -5,5 +17,5 @@ void	check_display_block_c(t_struct *form, va_list va)
 	if (form->l <= 0)
 		ft_putchar(va_arg(va, int));
 	else if (form->l > 0)
-		ft_putwchar(va_arg(va, int));
+		ft_putwchar(va_arg(va, wchar_t));
 }
