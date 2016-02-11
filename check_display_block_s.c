73370e47177bc3921b2dd9348c3f560f6e0af111
Jonathan LE QUERE <jle-quer@e1r10p18.42.fr>
2016-02-11 19:24:11 +0100

    Wchar_t Done.

--- check_display_block_s.c
+++ check_display_block_s.c
@@ -6,7 +6,7 @@
 /*   By: jle-quer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/11 19:11:12 by jle-quer          #+#    #+#             */
-/*   Updated: 2016/02/11 19:19:40 by jle-quer         ###   ########.fr       */
+/*   Updated: 2016/02/11 19:21:53 by jle-quer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 void	check_display_block_s(t_struct *form, va_list va)
 {
+	wchar_t	*str;
+
 	if (form->l <= 0 && form->type == 's')
 		ft_putstr(va_arg(va, char *));
 	else if (form->l > 0 || form->type == 'S')
 	{
-		wchar_t	*str;
 		str = va_arg(va, wchar_t*);
 		while (*str)
 		{
